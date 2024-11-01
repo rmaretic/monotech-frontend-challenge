849ab653bcd60a2fab02a2031342845c90f08d1a
Renato Maretić <renato.maretic@gmail.com>
2024-11-01 18:13:38 +0100

    Remove vue sample components

---
+++ src/components/CreationForm.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Github Cards</h1>
+        <h3>Make shareable github cards</h3>
+
+
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <CreationForm />
 </template>
+<script setup lang="ts">
+import CreationForm from '@/components/CreationForm.vue';
+
+</script>

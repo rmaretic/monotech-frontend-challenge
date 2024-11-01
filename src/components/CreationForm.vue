6b6c26314fa681ade4030dff39d01dafecdba530
Renato Maretić <renato.maretic@gmail.com>
2024-11-01 18:17:59 +0100

    Create card template, add dependencies

--- src/components/CreationForm.vue
+++ src/components/CreationForm.vue
@@ -3,11 +3,34 @@
         <h1>Github Cards</h1>
         <h3>Make shareable github cards</h3>
 
-
+        <!-- Card -->
+        <div class="card">
+          <div class="text-container">
+            <input type="text" placeholder="Github Username" class="text-input" v-model="username">
+            <span class="text-divider">/</span>
+            <input type="text" placeholder="Repo" class="text-input" v-model="repo">
+          </div>
+          <div class="flex space-between align-center">
+            <span class="section-title">Select template color:</span>
+            <div class="flex">
+                <color-picker v-model:pureColor="color"/>
+                <div class="color-text">
+                    {{ color }}
+                </div>
+            </div>
+          </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 
+import { ColorPicker } from "vue3-colorpicker";
+import "vue3-colorpicker/style.css";
+
+const username = ref('');
+const repo = ref('');
+const color = ref("rgb(40,203,164)");
 </script>
 <style>
 

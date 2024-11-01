3c68b7a3700a6e46ca89159b6ed6739d264ef283
Renato Maretić <renato.maretic@gmail.com>
2024-11-01 18:44:11 +0100

    Create GithubCard component

--- src/views/GithubPreviewView.vue
+++ src/views/GithubPreviewView.vue
@@ -1,5 +1,13 @@
 <template>
-    
+    <GithubCard/>
 </template>
 <script setup lang="ts">
+import GithubCard from '@/components/GithubCard.vue';
+import { useApiStore } from '@/store/api';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const apiStore = useApiStore();
+apiStore.getCard(route.params.id)
 </script>
